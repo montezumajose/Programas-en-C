857c0fac3f4af04f255a6c8b9f8a42292ab817ac
Jose_M <102838702+Jose2918@users.noreply.github.com>
2022-09-14 06:58:02 +0000

    Update Calcular la superficie de una esfera.c

--- Calcular la superficie de una esfera.c
+++ Calcular la superficie de una esfera.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
-    printf("Escribe el radio de la esfera: \n");
+    float radio;
+    float superficie;
+    float volumen;
+    
+    printf("Escribe el radio de la esfera: ");
+    scanf("%f",&radio);
+    
+    superficie = 4*3.141593*pow (radio, 2);
+    volumen = 4/3*3.141593*pow (radio, 3);    
+    
+    printf("La superficie de la esfera es de %.2f",superficie);
+    printf("\nEl volumen de la esfera es de %.2f",volumen);
     
     return 0;
 }
